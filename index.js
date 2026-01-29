9b924ff2bda4f2297b7b75988ec7da6f60dacffa
NandiniSoni123 <nandini2292soni@gmail.com>
2026-01-29 23:28:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,78 +10,105 @@ dotenv.config();
 
 const app = express();
 
-app.use(express.json());
-app.use(cors({
-  origin: "*", 
-  credentials: true
-}));
 connectDb();
 
-app.get("/", (req, res) => {
-  res.send("Backend running on Vercel 🚀");
-});
+app.use(express.json());
+app.use(cors({
+  origin: "*"
+}));
 
 app.use("/api/user", UserRoutes);
 app.use("/api/resume", ResumeRoutes);
+
 
 export default app;
 
 
 
-
-
-
-
-
 // import express from "express";
 // import dotenv from "dotenv";
-// dotenv.config();
-// import connectDb from './conn.js'
-// import mongoose from "mongoose";
-// import "./conn.js";
 // import cors from "cors";
-// // import path from 'path';
-// // import { fileURLToPath } from "url";
-
-
-
-
-
-// // const __filename = fileURLToPath(import.meta.url);
-// // const __dirname = path.dirname(__filename);
-
-
+// import connectDb from "./conn.js";
 
 // import UserRoutes from "./Routes/user.js";
 // import ResumeRoutes from "./Routes/resume.js";
 
+// dotenv.config();
+
 // const app = express();
-// const PORT = 4000;
 
 // app.use(express.json());
 // app.use(cors({
-//   credentials:true,
-//   origin:"http://localhost:5173"
-// }))
+//   origin: "*", 
+//   credentials: true
+// }));
+// connectDb();
 
-// // console.log("UserRoutes:", UserRoutes);
-// // console.log("ResumeRoutes:", ResumeRoutes);
+// app.get("/", (req, res) => {
+//   res.send("Backend running on Vercel 🚀");
+// });
 
 // app.use("/api/user", UserRoutes);
 // app.use("/api/resume", ResumeRoutes);
 
-// // Server static files from the react app's build folder
-// // app.use(express.static(path.join(__dirname,"build")));
+// export default app;
 
-// // Handle react routing,return index.html for all other requests
-// // app.get("/",(req,res)=>{
-// //   res.sendFile(path.join(__dirname,"build","index.html"));
+
+
+
+
+
+
+
+// // import express from "express";
+// // import dotenv from "dotenv";
+// // dotenv.config();
+// // import connectDb from './conn.js'
+// // import mongoose from "mongoose";
+// // import "./conn.js";
+// // import cors from "cors";
+// // // import path from 'path';
+// // // import { fileURLToPath } from "url";
+
+
+
+
+
+// // // const __filename = fileURLToPath(import.meta.url);
+// // // const __dirname = path.dirname(__filename);
+
+
+
+// // import UserRoutes from "./Routes/user.js";
+// // import ResumeRoutes from "./Routes/resume.js";
+
+// // const app = express();
+// // const PORT = 4000;
+
+// // app.use(express.json());
+// // app.use(cors({
+// //   credentials:true,
+// //   origin:"http://localhost:5173"
+// // }))
+
+// // // console.log("UserRoutes:", UserRoutes);
+// // // console.log("ResumeRoutes:", ResumeRoutes);
+
+// // app.use("/api/user", UserRoutes);
+// // app.use("/api/resume", ResumeRoutes);
+
+// // // Server static files from the react app's build folder
+// // // app.use(express.static(path.join(__dirname,"build")));
+
+// // // Handle react routing,return index.html for all other requests
+// // // app.get("/",(req,res)=>{
+// // //   res.sendFile(path.join(__dirname,"build","index.html"));
+// // // });
+
+// // app.listen(PORT, () => {
+// //   connectDb()
+// //   console.log("Backend created successfully on port", PORT);
 // // });
-
-// app.listen(PORT, () => {
-//   connectDb()
-//   console.log("Backend created successfully on port", PORT);
-// });
 
 
 
